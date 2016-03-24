c7ea779975359a2320f4ab1506fcb4b80037f1b3
imhuwq <imhuwq@gmail.com>
2016-03-24 22:06:14 +0800

    chapter 5

--- app/hello.py
+++ app/hello.py
@@ -1,18 +1,32 @@
 from flask import Flask, render_template, session, redirect, url_for, flash
-from flask.ext.script import Manager
+from flask.ext.script import Manager, Shell
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.moment import Moment
+from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.migrate import Migrate, MigrateCommand
 from flask.ext.wtf import Form
 from wtforms import StringField, SubmitField
 from wtforms.validators import Required
 from datetime import datetime
+import os
+
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 app = Flask(__name__)
+
 app.config['SECRET_KEY'] = 'NEVER TELL TO ANYBODY'
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+    'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 
 class NameForm(Form):
@@ -20,19 +34,44 @@ class NameForm(Form):
     submit = SubmitField('Submit')
 
 
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    users = db.relationship('User', backref='role', lazy='dynamic')
+
+    def __repr__(self):
+        return '<Role %r>' % self.name
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    def __repr__(self):
+        return '<User %r>' % self.name
+
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = NameForm()
+    user = User.query.filter_by(name=form.name.data).first()
     if form.validate_on_submit():
-        old_name = session.get('name')
-        if old_name is not None and old_name != form.name.data:
-            flash('Looks like you have changed your name.')
+        if user is None:
+            session['known'] = False
+            user = User(name=form.name.data)
+            db.session.add(user)
+        else:
+            session['known'] = True
         session['name'] = form.name.data
-        form.name.data = ''
+        form.name.data = ''  # this line seems unnecessary
         return redirect(url_for('index'))
     return render_template('index.html',
                            current_time=datetime.utcnow(),
                            name=session.get('name'),
+                           known=session.get('known'),
                            form=form)
 
 
@@ -50,6 +89,14 @@ def page_not_found(e):
 @app.errorhandler(500)
 def internel_server_error(e):
     return render_template('500.html'), 500
+
+
+manager.add_command('db', MigrateCommand)
+
+
+def make_shell_context():
+    return dict(app=app, db=db, User=User, Role=Role)
+manager.add_command('shell', Shell(make_context=make_shell_context))
 
 
 if __name__ == '__main__':
