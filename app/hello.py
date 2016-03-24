21393e00ac4a013a29ef0b2a0a8f217fd00c37de
imhuwq <imhuwq@gmail.com>
2016-03-24 13:48:43 +0800

    chapter 2

--- app/hello.py
+++ app/hello.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from flask.ext.script import Manager
 
 
 app = Flask(__name__)
+manager = Manager(app)
 
 
 @app.route('/')
@@ -9,5 +11,9 @@ def index():
     return '<h1>Hello World !</h1>'
 
 
+@app.route('/user/<name>')
+def user(name):
+    return '<h1>Hello %s</h1>' % name
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    manager.run()
