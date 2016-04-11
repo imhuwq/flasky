8f1dcaedc03979bbd2cda9e44c6e89d6eca5509f
imhuwq <imhuwq@gmail.com>
2016-04-11 10:42:26 +0800

    get environ vars from a local file

--- manage.py
+++ manage.py
@@ -4,6 +4,13 @@ from app.models import User, Role, Post
 from flask.ext.script import Manager, Shell
 from flask.ext.migrate import Migrate, MigrateCommand
 
+if os.path.exists('.env'):
+    print('Importing environment from .env...')
+    for line in open('.env'):
+        var = line.strip().split('=')
+        if len(var) == 2:
+            os.environ[var[0]] = var[1]
+
 app = create_app(os.environ.get('FLAKY_CONFIG') or 'default')
 manager = Manager(app)
 migrate = Migrate(app, db)
